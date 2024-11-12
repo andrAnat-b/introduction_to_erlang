9f64852fe80a7130b8999744b94bd2caa23e9cc1
vadymkykalo <vadymkykalo@gmail.com>
2024-11-12 23:29:29 +0200

    impl 14-15 p

---
+++ utils_p14.erl
@@ -0,0 +1,9 @@
+-module(utils_p14).
+-author("vkykalo").
+-export([dupli/1]).
+
+% * (dupli '(a b c c d))
+% (A A B B C C C C D D)
+
+dupli([]) -> [];
+dupli([H | T]) -> [H, H] ++ dupli(T).
---
+++ utils_p15.erl
@@ -0,0 +1,9 @@
+-module(utils_p15).
+-author("vkykalo").
+-export([repli/2]).
+
+repli([], _) -> [];
+repli([H | T], Count) -> multi(H, Count) ++ repli(T, Count).
+
+multi(_, 0) -> [];
+multi(H, T) -> [H | multi(H, T - 1)].
